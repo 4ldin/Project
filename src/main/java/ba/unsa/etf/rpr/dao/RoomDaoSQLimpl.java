cc8cb296fbf6bba82eac880e27f1a4bc972790f7
4ldin <aislamagic1@etf.unsa.ba>
2022-12-11 22:30:28 +0100

    Templates for SQL DAO implementation

---
+++ src/main/java/ba/unsa/etf/rpr/dao/RoomDaoSQLimpl.java
@@ -0,0 +1,38 @@
+package ba.unsa.etf.rpr.dao;
+
+import ba.unsa.etf.rpr.domain.RoomTypes;
+import ba.unsa.etf.rpr.domain.Rooms;
+
+import java.util.List;
+
+public class RoomDaoSQLimpl implements RoomDao{
+    @Override
+    public Rooms getById(int id) {
+        return null;
+    }
+
+    @Override
+    public Rooms add(Rooms item) {
+        return null;
+    }
+
+    @Override
+    public Rooms update(Rooms item) {
+        return null;
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
+
+    @Override
+    public List<Rooms> getAll() {
+        return null;
+    }
+
+    @Override
+    public List<Rooms> searchByRoomType(RoomTypes roomType) {
+        return null;
+    }
+}
