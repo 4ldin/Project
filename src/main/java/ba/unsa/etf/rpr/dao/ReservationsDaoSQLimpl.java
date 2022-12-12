99e938b535c43ca2d74f838c5e7efa5326280007
4ldin <aislamagic1@etf.unsa.ba>
2022-12-12 22:31:08 +0100

    Added constructor for ReservationsDaoSQLimpl

--- src/main/java/ba/unsa/etf/rpr/dao/ReservationsDaoSQLimpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/ReservationsDaoSQLimpl.java
@@ -2,9 +2,26 @@ package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Reservations;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.util.List;
 
 public class ReservationsDaoSQLimpl implements ReservationsDao{
+
+    private Connection connection;
+
+    /**
+     * Constructor for ReservationsDaoSQLimpl that connects to the database
+     * @param connection
+     */
+    public ReservationsDaoSQLimpl(Connection connection) {
+        try{
+            this.connection = DriverManager.getConnection("jdbc://sql7.freemysqlhosting.net:3306/user", "user", "password");
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
     @Override
     public Reservations getById(int id) {
         return null;
