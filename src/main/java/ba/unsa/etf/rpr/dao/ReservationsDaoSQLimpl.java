18f3715c10ec8758083d48c595799974c7e2ed73
4ldin <aislamagic1@etf.unsa.ba>
2022-12-12 23:58:35 +0100

    Added getGuestById in ReservationsDaoSQLimpl

--- src/main/java/ba/unsa/etf/rpr/dao/ReservationsDaoSQLimpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/ReservationsDaoSQLimpl.java
@@ -1,9 +1,9 @@
 package ba.unsa.etf.rpr.dao;
 
+import ba.unsa.etf.rpr.domain.Guests;
 import ba.unsa.etf.rpr.domain.Reservations;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.*;
 import java.util.List;
 
 public class ReservationsDaoSQLimpl implements ReservationsDao{
@@ -22,6 +22,40 @@ public class ReservationsDaoSQLimpl implements ReservationsDao{
         }
     }
 
+    /**
+     * Method that returns guest for given id of a reservation
+     * @param id of guest
+     * @return Guest with given id
+     */
+    public Guests getGuestById(int id){
+        String query = "SELECT * FROM Guests WHERE id = (SELECT r.Guest_id FROM Reservations r WHERE r.Reservation_id = ?)";
+        try {
+            PreparedStatement stmt = this.connection.prepareStatement(query);
+            stmt.setInt(1, id);
+            ResultSet rs = stmt.executeQuery();
+            if(rs.next()){
+                Guests guest = new Guests();
+                guest.setId(rs.getInt("id"));
+                guest.setFirstName(rs.getString("First_name"));
+                guest.setLastName(rs.getString("Last_Name"));
+                guest.setPhone(rs.getInt("Phone"));
+                guest.setPassportNumber(rs.getInt("Passport_number"));
+                rs.close();
+                return guest;
+            }else{
+                return null;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * Method that finds Reservation with mathcing id
+     * @param id primary key of entity
+     * @return Reservation with given id
+     */
     @Override
     public Reservations getById(int id) {
         return null;
