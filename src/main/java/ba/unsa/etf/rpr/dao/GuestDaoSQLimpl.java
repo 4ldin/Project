dc5578712f66536c96f23934b1b02561e008af05
4ldin <aislamagic1@etf.unsa.ba>
2022-12-12 01:07:38 +0100

    Added constructor to GuestDaoSQLimpl

--- src/main/java/ba/unsa/etf/rpr/dao/GuestDaoSQLimpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/GuestDaoSQLimpl.java
@@ -2,11 +2,24 @@ package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Guests;
 
+import java.sql.*;
 import java.util.List;
 
 public class GuestDaoSQLimpl implements GuestDao{
 
+    private Connection connection;
 
+    /**
+     * Constructor for GuestDao that connects to the database
+     * @param connection
+     */
+    public GuestDaoSQLimpl(Connection connection) {
+        try{
+            this.connection = DriverManager.getConnection("jdbc://sql7.freemysqlhosting.net:3306/user", "user", "password");
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
 
     @Override
     public Guests getById(int id) {
