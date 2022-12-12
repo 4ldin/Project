0e79124b37e17be4a09f08eba7ad1977a6176a4d
4ldin <aislamagic1@etf.unsa.ba>
2022-12-13 00:54:07 +0100

    Added constructor in RoomDaoSQLimpl

--- src/main/java/ba/unsa/etf/rpr/dao/RoomDaoSQLimpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/RoomDaoSQLimpl.java
@@ -3,9 +3,26 @@ package ba.unsa.etf.rpr.dao;
 import ba.unsa.etf.rpr.domain.RoomTypes;
 import ba.unsa.etf.rpr.domain.Rooms;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.util.List;
 
 public class RoomDaoSQLimpl implements RoomDao{
+
+    private Connection connection;
+
+    /**
+     * Constructor for GuestDao that connects to the database
+     * @param connection
+     */
+    public RoomDaoSQLimpl(Connection connection) {
+        try{
+            this.connection = DriverManager.getConnection("jdbc://sql7.freemysqlhosting.net:3306/user", "user", "password");
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
     @Override
     public Rooms getById(int id) {
         return null;
