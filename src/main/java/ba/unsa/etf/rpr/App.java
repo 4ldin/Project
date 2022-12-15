777fcd4a70d86ce88603323f8750f8f437afbddb
4ldin <aislamagic1@etf.unsa.ba>
2022-12-15 01:31:54 +0100

    Small test of database connection

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -1,5 +1,14 @@
 package ba.unsa.etf.rpr;
 
+import ba.unsa.etf.rpr.dao.GuestDao;
+import ba.unsa.etf.rpr.dao.GuestDaoSQLimpl;
+import ba.unsa.etf.rpr.domain.Guests;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+
 /**
  * Hello world!
  *
@@ -8,6 +17,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Guests guest = new Guests();
+        GuestDao dao = new GuestDaoSQLimpl();
+        List<Guests> guests = dao.getAll();
+        System.out.println(guests);
     }
 }
