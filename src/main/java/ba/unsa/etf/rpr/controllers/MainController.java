f63f30a9d15de86bf5fbfd00dc80d6d23e73d834
4ldin <aislamagic1@etf.unsa.ba>
2022-12-22 07:37:49 +0100

    Added login check feature for login controller

--- src/main/java/ba/unsa/etf/rpr/controllers/MainController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/MainController.java
@@ -1,9 +1,17 @@
 package ba.unsa.etf.rpr.controllers;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
 
 public class MainController {
 
+    public TextField fieldUsername;
+
     public void loginClick(ActionEvent actionEvent) {
+        if(fieldUsername.getText().isEmpty()){
+            fieldUsername.getStyleClass().add("wrongField");
+            System.out.println("Empty username!");
+            return;
+        }
     }
 }
