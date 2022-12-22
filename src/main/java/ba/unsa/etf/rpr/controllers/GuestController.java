0428cda354f05fec7e00c21a7bd97c9aae943a0b
4ldin <aislamagic1@etf.unsa.ba>
2022-12-23 00:38:56 +0100

    Added logout button in GuestController

--- src/main/java/ba/unsa/etf/rpr/controllers/GuestController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/GuestController.java
@@ -1,5 +1,12 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+
 public class GuestController {
 
+    public Button logoutButton;
+
+    public void logoutClick(ActionEvent actionEvent) {
+    }
 }
