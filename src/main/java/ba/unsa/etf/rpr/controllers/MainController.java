ece51a7d2cad252fe3e3f18b5e520f3cc848a79c
4ldin <aislamagic1@etf.unsa.ba>
2022-12-22 07:47:13 +0100

    Added initialize for MainController

--- src/main/java/ba/unsa/etf/rpr/controllers/MainController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/MainController.java
@@ -1,11 +1,31 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 
 public class MainController {
 
     public TextField fieldUsername;
+
+    @FXML
+    public void initialize(){
+        fieldUsername.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observableValue, String o, String n) {
+                if (fieldUsername.getText().trim().isEmpty()) {
+                    fieldUsername.getStyleClass().removeAll("correctField");
+                    fieldUsername.getStyleClass().add("wrongField");
+                } else {
+                    fieldUsername.getStyleClass().removeAll("wrongField");
+                    fieldUsername.getStyleClass().add("correctField");
+                }
+            }
+        }
+        );
+    }
 
     public void loginClick(ActionEvent actionEvent) {
         if(fieldUsername.getText().isEmpty()){
