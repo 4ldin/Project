6cc6f163edac0c9359fc603b6212b782b5b5cf6e
4ldin <aislamagic1@etf.unsa.ba>
2022-12-22 07:57:16 +0100

    Added new window when login button is pressed

--- src/main/java/ba/unsa/etf/rpr/controllers/MainController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/MainController.java
@@ -4,7 +4,15 @@ import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
 public class MainController {
 
@@ -27,11 +35,16 @@ public class MainController {
         );
     }
 
-    public void loginClick(ActionEvent actionEvent) {
+    public void loginClick(ActionEvent actionEvent) throws IOException {
         if(fieldUsername.getText().isEmpty()){
             fieldUsername.getStyleClass().add("wrongField");
             System.out.println("Empty username!");
             return;
         }
+        Stage stage = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Guest.fxml"));
+        stage.setTitle("Hotel Reservation System");
+        stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        stage.show();
     }
 }
