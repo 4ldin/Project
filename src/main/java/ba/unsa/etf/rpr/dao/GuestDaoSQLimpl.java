ec6fe786f9c09ca8ec5d55c1e4adfb5749b73544
4ldin <aislamagic1@etf.unsa.ba>
2022-12-22 09:33:06 +0100

    Added getByEmailPassword method in GuestDaoSQLimpl

--- src/main/java/ba/unsa/etf/rpr/dao/GuestDaoSQLimpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/GuestDaoSQLimpl.java
@@ -163,4 +163,34 @@ public class GuestDaoSQLimpl implements GuestDao{
         }
         return guests;
     }
+
+    /**
+     * Method that finds guest with given email and password
+     * @param email email of guest
+     * @param password password of guest
+     * @return guest
+     */
+    public Guests getByEmailPassword(String email, String password){
+        String query = "SELECT * FROM Guests WHERE eMail = ? AND password = ?";
+        try {
+            PreparedStatement stmt = this.connection.prepareStatement(query);
+            stmt.setString(1,email);
+            stmt.setString(2,password);
+            ResultSet rs = stmt.executeQuery();
+            Guests guest = new Guests();
+            while(rs.next()){
+                guest.setId(rs.getInt("Guest_id"));
+                guest.setFirstName(rs.getString("First_name"));
+                guest.setLastName(rs.getString("Last_name"));
+                guest.setPhone(rs.getInt("Phone"));
+                guest.setPassportNumber(rs.getInt("Passport_number"));
+                guest.seteMail(rs.getString("eMail"));
+                guest.setPassword(rs.getString("password"));
+            }
+            rs.close();
+            return guest;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
