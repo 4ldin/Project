ea0c58b72b393032254134edc952a7657f60ba1f
4ldin <aislamagic1@etf.unsa.ba>
2022-12-23 15:24:20 +0100

    Switched to BorderPane for Guest window

--- src/main/java/ba/unsa/etf/rpr/controllers/GuestController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/GuestController.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.MenuItem;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,6 +14,7 @@ import java.io.IOException;
 import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
 public class GuestController {
+
 
     public Button logoutButton;
 
