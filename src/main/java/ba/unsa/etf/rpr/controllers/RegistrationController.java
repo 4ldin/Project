4b4acb4c04025ecb9678703652927a47cc5ce2c8
4ldin <aislamagic1@etf.unsa.ba>
2022-12-26 15:45:41 +0100

    Added listeners in RegistrationController

--- src/main/java/ba/unsa/etf/rpr/controllers/RegistrationController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/RegistrationController.java
@@ -1,4 +1,22 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class RegistrationController {
+    public TextField firstName;
+    public Label lastName;
+    public TextField eMail;
+    public TextField password;
+
+    @FXML
+    void initialize(){
+        firstName.textProperty().addListener((observableValue, oldVal, newVal) -> {});
+        lastName.textProperty().addListener((observableValue, oldVal, newVal) -> {});
+        eMail.textProperty().addListener((observableValue, oldVal, newVal) -> {});
+        password.textProperty().addListener((observableValue, oldVal, newVal) -> {});
+    }
+
+
 }
