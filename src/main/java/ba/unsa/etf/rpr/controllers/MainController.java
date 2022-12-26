87460ca5868d4045e922df925a1140d79a1e886a
4ldin <aislamagic1@etf.unsa.ba>
2022-12-26 13:23:53 +0100

    Implemented register button that opens registration window

--- src/main/java/ba/unsa/etf/rpr/controllers/MainController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/MainController.java
@@ -105,4 +105,16 @@ public class MainController {
             }
         }
     }
+
+    public void registerClick(ActionEvent actionEvent) throws IOException {
+        Stage stage = new Stage();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/registration.fxml"));
+        Parent root = loader.load();
+        stage.setTitle("Registration");
+        stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        stage.setResizable(false);
+        stage.show();
+        Stage primaryStage = (Stage) loginButton.getScene().getWindow();
+        primaryStage.hide();
+    }
 }
