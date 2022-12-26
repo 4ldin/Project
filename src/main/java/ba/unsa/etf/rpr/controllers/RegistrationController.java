ec8c87d9decb4ada6f828195a3a34f9068ed850b
4ldin <aislamagic1@etf.unsa.ba>
2022-12-26 20:01:04 +0100

    Implemented return button in RegistrationController

--- src/main/java/ba/unsa/etf/rpr/controllers/RegistrationController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/RegistrationController.java
@@ -5,12 +5,18 @@ import ba.unsa.etf.rpr.dao.GuestDaoSQLimpl;
 import ba.unsa.etf.rpr.domain.Guests;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.util.List;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
 public class RegistrationController {
     public TextField firstName;
@@ -66,6 +72,17 @@ public class RegistrationController {
     }
 
 
-    public void returnClick(ActionEvent actionEvent) {
+    public void returnClick(ActionEvent actionEvent) throws IOException {
+        Stage primaryStage = new Stage();
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/main.fxml"));
+        Parent root =  fxmlLoader.load();
+        Scene scene = new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE);
+        primaryStage.setTitle("Hotel Reservation System");
+        primaryStage.setResizable(false);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        Stage guestStage = (Stage) returnButton.getScene().getWindow();
+        guestStage.close();
+        System.out.println("Returning to login screen.");
     }
 }
