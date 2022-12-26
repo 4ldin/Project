e2369973cc8030d4e3956755a04434c2c07fc336
4ldin <aislamagic1@etf.unsa.ba>
2022-12-26 18:02:03 +0100

    Implemented listener for first name and last name label in RegistrationController

--- src/main/java/ba/unsa/etf/rpr/controllers/RegistrationController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/RegistrationController.java
@@ -6,15 +6,34 @@ import javafx.scene.control.TextField;
 
 public class RegistrationController {
     public TextField firstName;
-    public Label lastName;
+    
     public TextField eMail;
     public TextField password;
+    public TextField lastName;
 
     @FXML
     void initialize(){
-        firstName.textProperty().addListener((observableValue, oldVal, newVal) -> {});
-        lastName.textProperty().addListener((observableValue, oldVal, newVal) -> {});
-        eMail.textProperty().addListener((observableValue, oldVal, newVal) -> {});
+        firstName.textProperty().addListener((observableValue, oldVal, newVal) -> {
+            if(firstName.getText().trim().isEmpty()){
+                firstName.getStyleClass().removeAll("correctField");
+                firstName.getStyleClass().add("wrongField");
+            }else{
+                firstName.getStyleClass().removeAll("wrongField");
+                firstName.getStyleClass().add("correctField");
+            }
+        });
+        lastName.textProperty().addListener((observableValue, oldVal, newVal) -> {
+            if(lastName.getText().trim().isEmpty()){
+                lastName.getStyleClass().removeAll("correctField");
+                lastName.getStyleClass().add("wrongField");
+            }else{
+                lastName.getStyleClass().removeAll("wrongField");
+                lastName.getStyleClass().add("correctField");
+            }
+        });
+        eMail.textProperty().addListener((observableValue, oldVal, newVal) -> {
+
+        });
         password.textProperty().addListener((observableValue, oldVal, newVal) -> {});
     }
 
