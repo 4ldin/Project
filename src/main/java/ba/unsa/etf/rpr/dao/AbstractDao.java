d8aedc60fe88245364fc3cc659f40bd4dc39a35b
4ldin <aislamagic1@etf.unsa.ba>
2022-12-29 15:32:16 +0100

    Added constructor in AbstractDao

--- src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
@@ -2,11 +2,39 @@ package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Idable;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 /**
  * Abstract class that implements core DAO CRUD methods for every entity
  * @author Aldin Islamagic
  */
 
 public abstract class AbstractDao<T extends Idable> implements Dao<T> {
+
+    private Connection connection;
+    private String tableName;
+
+    /**
+     * Constructor for abstract dao that makes connection to database with hidden db name and password
+     * @param tableName name of table in database
+     */
+    public AbstractDao(String tableName){
+        this.tableName = tableName;
+        try {
+            String fieldPath = "src/dataBase.properties";
+            Properties pros = new Properties();
+            FileInputStream ip = new FileInputStream(fieldPath);
+            pros.load(ip);
+            this.connection = DriverManager.getConnection(pros.getProperty("url"), pros.getProperty("username"), pros.getProperty("password"));
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
 
 }
