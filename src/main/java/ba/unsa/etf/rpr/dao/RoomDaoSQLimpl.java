63c09f9482bc8bd3f63bd6c02aec5a846c2ced5b
4ldin <aislamagic1@etf.unsa.ba>
2022-12-29 22:57:19 +0100

    Refactored RoomDaoSQLimpl

--- src/main/java/ba/unsa/etf/rpr/dao/RoomDaoSQLimpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/RoomDaoSQLimpl.java
@@ -11,30 +11,33 @@ import java.io.IOException;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 
-public class RoomDaoSQLimpl implements RoomDao{
+public class RoomDaoSQLimpl extends AbstractDao<Rooms> implements RoomDao{
 
-    private Connection connection;
 
     /**
-     * Constructor for GuestDao that connects to the database
+     * Constructor for RoomDao that connects to the database
      */
-    public RoomDaoSQLimpl() throws IOException {
-        String fieldPath = "src/dataBase.properties";
-        Properties pros = new Properties();
-        FileInputStream ip = new FileInputStream(fieldPath);
-        pros.load(ip);
-        try{
-            this.connection = DriverManager.getConnection(pros.getProperty("url"), pros.getProperty("username"), pros.getProperty("password"));
-        }catch(Exception e){
-            e.printStackTrace();
-        }
+    public RoomDaoSQLimpl(){
+        super("Rooms");
     }
+
+    @Override
+    public Rooms row2object(ResultSet rs) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public Map<String, Object> object2row(Rooms object) {
+        return null;
+    }
+
     public Guests getGuestById(int id){
         String query = "SELECT * FROM Guests WHERE id = (SELECT r.Guest_id FROM Reservations r WHERE r.Reservation_id = ?)";
         try {
-            PreparedStatement stmt = this.connection.prepareStatement(query);
+            PreparedStatement stmt = getConnection().prepareStatement(query);
             stmt.setInt(1, id);
             ResultSet rs = stmt.executeQuery();
             if(rs.next()){
@@ -61,7 +64,7 @@ public class RoomDaoSQLimpl implements RoomDao{
     public Reservations getReservationById(int id){
         String query = "SELECT * FROM Reservations WHERE id = (SELECT r.Reservations_id FROM Rooms r WHERE r.Room_id = ?)";
         try {
-            PreparedStatement stmt = this.connection.prepareStatement(query);
+            PreparedStatement stmt = getConnection().prepareStatement(query);
             stmt.setInt(1, id);
             ResultSet rs = stmt.executeQuery();
             if(rs.next()){
@@ -91,7 +94,7 @@ public class RoomDaoSQLimpl implements RoomDao{
     public RoomTypes getRoomTypeById(int id){
         String query = "SELECT * FROM Room_Types WHERE id = (SELECT r.Room_type_id FROM Rooms r WHERE r.Room_id = ?)";
         try {
-            PreparedStatement stmt = this.connection.prepareStatement(query);
+            PreparedStatement stmt = getConnection().prepareStatement(query);
             stmt.setInt(1, id);
             ResultSet rs = stmt.executeQuery();
             if(rs.next()){
@@ -110,119 +113,6 @@ public class RoomDaoSQLimpl implements RoomDao{
         return null;
     }
 
-    /**
-     * Method that finds Room with matching id
-     * @param id primary key of entity
-     * @return Room with given id
-     */
-    @Override
-    public Rooms getById(int id) {
-        String query = "SELECT * FROM Rooms WHERE Room_id = ?";
-        try {
-            PreparedStatement stmt = this.connection.prepareStatement(query);
-            stmt.setInt(1, id);
-            ResultSet rs = stmt.executeQuery();
-            if(rs.next()){
-                Rooms room = new Rooms();
-                room.setId(rs.getInt("Room_id"));
-                room.setOccupancy(rs.getBoolean("Occupancy"));
-                room.setReservation(getReservationById(id));
-                room.setRoomType(getRoomTypeById(id));
-                rs.close();
-                return room;
-            }else {
-                return null;
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    /**
-     * Method that adds given room in database
-     * @param item item to save in database
-     * @return
-     */
-    @Override
-    public Rooms add(Rooms item) {
-        String insert = "INSERT INTO Rooms (Occupancy, Reservations_id, Room_type_id) VALUES(?, ?, ?)";
-        try {
-            PreparedStatement stmt = this.connection.prepareStatement(insert);
-            stmt.setBoolean(1, item.getOccupancy());
-            stmt.setInt(2, item.getReservation().getId());
-            stmt.setInt(3, item.getRoomType().getId());
-            stmt.executeUpdate(insert);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    /**
-     * Method that updates a room
-     * @param item - item to be updated
-     * @param id - id of room to be updated
-     * @return updated room
-     */
-    @Override
-    public Rooms update(Rooms item, int id) {
-        String query = "UPDATE Rooms SET Occupancy = ?, Reservations_id = ?, Room_type_id = ? WHERE Room_id = ?";
-        try {
-            PreparedStatement stmt = this.connection.prepareStatement(query);
-            stmt.setBoolean(1, item.getOccupancy());
-            stmt.setInt(2, item.getReservation().getId());
-            stmt.setInt(3, item.getRoomType().getId());
-            stmt.setInt(4, id);
-            stmt.executeUpdate(query);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    /**
-     * Method that deletes room with matching id
-     * @param id - primary key of entity
-     */
-    @Override
-    public void delete(int id) {
-        String query = "DELETE FROM Rooms WHERE Room_id = ?";
-        try {
-            PreparedStatement stmt = this.connection.prepareStatement(query);
-            stmt.setInt(1, id);
-            stmt.executeUpdate(query);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    /**
-     * Lists all rooms from database
-     * @return List of all rooms from database
-     */
-    @Override
-    public List<Rooms> getAll() {
-        String query = "SELECT * FROM Rooms";
-        List<Rooms> rooms = new ArrayList<>();
-        try {
-            PreparedStatement stmt = this.connection.prepareStatement(query);
-            ResultSet rs = stmt.executeQuery();
-            while(rs.next()){
-                Rooms room = new Rooms();
-                room.setId(rs.getInt("Room_id"));
-                room.setOccupancy(rs.getBoolean("Occupancy"));
-                room.setReservation(getReservationById(room.getId()));
-                room.setRoomType(getRoomTypeById(room.getId()));
-                rooms.add(room);
-            }
-            rs.close();
-            return rooms;
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return rooms;
-    }
 
     @Override
     public List<Rooms> searchByRoomType(RoomTypes roomType) {
