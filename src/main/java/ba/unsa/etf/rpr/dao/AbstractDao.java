f2f9c259613d1cfa0aa2a688eb55d5505e7210a9
4ldin <aislamagic1@etf.unsa.ba>
2022-12-29 15:22:11 +0100

    Added abstract class for dao

---
+++ src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
@@ -0,0 +1,12 @@
+package ba.unsa.etf.rpr.dao;
+
+import ba.unsa.etf.rpr.domain.Idable;
+
+/**
+ * Abstract class that implements core DAO CRUD methods for every entity
+ * @author Aldin Islamagic
+ */
+
+public abstract class AbstractDao<T extends Idable> implements Dao<T> {
+
+}
