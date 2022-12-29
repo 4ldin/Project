e9fa0c216dddbaed8d08f17a9ea670a34961f0f3
4ldin <aislamagic1@etf.unsa.ba>
2022-12-29 15:53:18 +0100

    Added row2object and object2row in AbstractDao

--- src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
@@ -1,13 +1,16 @@
 package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Idable;
+import jdk.jshell.spi.ExecutionControl;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Map;
 import java.util.Properties;
 
 /**
@@ -52,5 +55,19 @@ public abstract class AbstractDao<T extends Idable> implements Dao<T> {
     public void setConnection(Connection connection){
         this.connection = connection;
     }
+
+    /**
+     * Method for mapping ResultSet into Object
+     * @param rs result set from database
+     * @return a bean object for a specific table
+     */
+    public abstract T row2object(ResultSet rs) throws SQLException;
+
+    /**
+     * Mehtod for mapping Object into Map
+     * @param object a bean object for a specific table
+     * @return value of object in a sorted map
+     */
+    public abstract Map<String, Object> object2row(T object);
 
 }
