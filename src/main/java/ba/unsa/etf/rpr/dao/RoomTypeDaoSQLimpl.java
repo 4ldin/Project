2c48d4f7bc3d321349b719488fcfb6eeefe25c6b
4ldin <aislamagic1@etf.unsa.ba>
2022-12-30 17:31:03 +0100

    Refactored RoomTypeDaoSQLimpl

--- src/main/java/ba/unsa/etf/rpr/dao/RoomTypeDaoSQLimpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/RoomTypeDaoSQLimpl.java
@@ -1,23 +1,16 @@
 package ba.unsa.etf.rpr.dao;
 
-import ba.unsa.etf.rpr.domain.Reservations;
 import ba.unsa.etf.rpr.domain.RoomTypes;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
-import java.util.Properties;
 
-public class RoomTypeDaoimpl extends AbstractDao<RoomTypes> implements RoomTypeDao{
+public class RoomTypeDaoSQLimpl extends AbstractDao<RoomTypes> implements RoomTypeDao{
 
     /**
      * Constructor for RoomTypeDaoimpl that connects to the database
      */
-    public RoomTypeDaoimpl(){
+    public RoomTypeDaoSQLimpl(){
         super("Room_Types");
     }
 
